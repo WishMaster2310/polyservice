d6ab91688f85f4e3015b34703df2b174b7265da1
wishmaster <wishmaster2310@gmail.com>
2015-12-27 12:49:07 +0500

    add works

---
+++ public/javascripts/page.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	// init mosaiflow
+	// docs here --> [https://github.com/sapegin/jquery.mosaicflow/blob/master/Readme.md]
+	$('.works').mosaicflow({
+	    itemSelector: '.works__item',
+	    minItemWidth: 300,
+	    columnClass: 'works__col'
+	});
+
+});
